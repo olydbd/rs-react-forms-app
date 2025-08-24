29f2f28a796273917b0f296c737f927a05c1dbd4
Olya Dubodel <dubodel.olya@gmail.com>
2025-08-24 23:16:00 +0300

    chore: setup react project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <h1 className="text-6xl font-bold text-pink-500">React Forms App</h1>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: '/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+        'src/__tests__',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
+});
